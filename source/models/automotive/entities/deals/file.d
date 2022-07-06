8fb186ffbe2873270eec956978a4331af6006c8f
UI-Manufaktur <ui.manufaktur@gmail.com>
2022-07-06 19:38:37 +0200

    Updates

--- source/models/automotive/entities/deals/file.d
+++ source/models/automotive/entities/deals/file.d
@@ -11,12 +11,16 @@ class DATMDealFile : DOOPEntity {
     super.initialize;
 
     this
-      .addValues([
-        "createdOnBehalfBy": UUIDAttribute, //Unique identifier of the delegate user who created the record."]),
-        "modifiedOnBehalfBy": UUIDAttribute, //Unique identifier of the delegate user who modified the record."]),
+      .addValues([ // fix values
+        CreatedOnBehalfByAttribute, 
+        ModifiedOnBehalfByAttribute, 
+        OwnerIdAttribute, 
+        StateCodeAttribute, 
+        StatusCodeAttribute 
+      ])
+      .addValues([ // individual values
         "overriddenCreatedOn": TimestampAttribute, //Date and time that the record was migrated."]),
         "importSequenceNumber": IntegerAttribute, //Unique identifier of the data import or data migration that created this record."]),
-        "ownerId": UUIDAttribute, // Owner Id"]),
         "ownerIdType": StringAttribute, // The type of owner, either User or Team."]),
         "owningBusinessUnitId": UUIDAttribute, //Unique identifier for the business unit that owns the record"]),
         "owningUserId": UUIDAttribute, //Unique identifier of the user that owns the activity."]),
@@ -25,28 +29,16 @@ class DATMDealFile : DOOPEntity {
         "utcConversionTimeZoneCode": StringAttribute, // Time zone code that was in use when the record was created."]),
         "dealFileId": UUIDAttribute, // Unique identifier for entity instances"]),
         "opportunityId": UUIDAttribute, // Parent opportunity of the deal."]),
-        "stateCode": StringAttribute, // Status of the Deal File"]),
-        "stateCode_display": StringAttribute, //
-        "statusCode": StringAttribute, // Reason for the status of the Deal File"]),
-        "statusCode_display": StringAttribute, //
       ])
-      .registerPath("automotive_dealfiles");
+      .registerPath("automotive_deals.files");
   }
 }
 mixin(EntityCalls!("ATMDealFile"));
 
 version(test_library) {
   unittest {
-    assert(APLFeedback);
     assert(ATMDealFile);
 
-  auto entity = ATMDealFile;
-  // auto repository = OOPFileRepository("./tests");
-/*  repository.create("entities", entity.entityClasses, entity.toJson);
-
-  auto json = repository.findOne("entities", entity.entityClasses, ["id":entity.id.toString]);
-  assert(json != Json(null), entity.id.toString~" not found");
-
-  repository.cleanupConnections; */
+    auto entity = ATMDealFile;
   }
 }
--- source/models/automotive/entities/deals/term.d
+++ source/models/automotive/entities/deals/term.d
@@ -4,19 +4,23 @@ module models.automotive.entities.deals.term;
 import models.automotive;
 
 // Terms applicable a given type of deal.
-class DATMDealTerm : DOOPEntity {
-  mixin(EntityThis!("ATMDealTerm"));
+class DDealTermEntity : DOOPEntity {
+  mixin(EntityThis!("DealTermEntity"));
   
   override void initialize() {
     super.initialize;
 
     this
-      .addValues([
-        "createdOnBehalfBy": UUIDAttribute, //Unique identifier of the delegate user who created the record."]),
-        "modifiedOnBehalfBy": UUIDAttribute, //Unique identifier of the delegate user who modified the record."]),
+      .addValues([ // fix values
+        CreatedOnBehalfByAttribute, 
+        ModifiedOnBehalfByAttribute, 
+        OwnerIdAttribute, 
+        StateCodeAttribute, 
+        StatusCodeAttribute 
+      ])
+      .addValues([ // individual values
         "overriddenCreatedOn": TimestampAttribute, //Date and time that the record was migrated."]),
         "importSequenceNumber": IntegerAttribute, //Unique identifier of the data import or data migration that created this record."]),
-        "ownerId": UUIDAttribute, // Owner Id"]),
         "ownerIdType": StringAttribute, // The type of owner, either User or Team."]),
         "owningBusinessUnitId": UUIDAttribute, //Unique identifier for the business unit that owns the record"]),
         "owningUserId": UUIDAttribute, //Unique identifier of the user that owns the activity."]),
@@ -25,28 +29,16 @@ class DATMDealTerm : DOOPEntity {
         "utcConversionTimeZoneCode": StringAttribute, // Time zone code that was in use when the record was created."]),
         "dealTermId": UUIDAttribute, // Unique identifier for entity instances"]),
         "dealTypeId": UUIDAttribute, // Type of deal associated with these terms."]),
-        "stateCode": StringAttribute, // Status of the Deal Term"]),
-        "stateCode_display": StringAttribute, //
-        "statusCode": StringAttribute, // Reason for the status of the Deal Term"]),
-        "statusCode_display": StringAttribute, //
       ])
-      .registerPath("automotive_dealterms");
+      .registerPath("automotive_deals.terms");
   }
 }
-mixin(EntityCalls!("ATMDealTerm"));
+mixin(EntityCalls!("DealTermEntity"));
 
 version(test_library) {
   unittest {
-    assert(APLFeedback);
-    assert(ATMDealTerm);
+    assert(DealTermEntity);
 
-  auto entity = ATMDealTerm;
-  // auto repository = OOPFileRepository("./tests");
-/*  repository.create("entities", entity.entityClasses, entity.toJson);
-
-  auto json = repository.findOne("entities", entity.entityClasses, ["id":entity.id.toString]);
-  assert(json != Json(null), entity.id.toString~" not found");
-
-  repository.cleanupConnections; */
+    auto entity = DealTermEntity;
   }
 }
